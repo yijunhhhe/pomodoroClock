4b5a3ae0b91067c3f251638a230822961cbdfc60
yijunhhhe <yijunhhhe@gmail.com>
2017-09-16 10:38:34 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,121 @@
-$(document).ready(function(){
+$(function(){
+    var breaklength = 5 * 60;
+    var sessionlength = 25 * 60;
+    var timeLeft = sessionlength;
+    var isSession = true;
+    var timeInterval;
+    var clickInterval
+    
+    function switchSession(){
+        if(isSession){
+            timeLeft = breaklength;
+            isSession = false;
+        }else{
+            timeLeft = sessionlength;
+            isSession = true;
+        }
+    }
+
+    function formatSeconds(seconds) {
+        return seconds < 10 ? '0' + seconds : seconds;
+    }
+
+    function updateTimer(){
+        var minutesLeft;
+        var secondsLeft;
+
+        minutesLeft = Math.floor(timeLeft / 60);
+        secondsLeft = timeLeft - minutesLeft * 60;
+        $(".time-left").html(minutesLeft + ':' + formatSeconds(secondsLeft));  
+    }
+
+    function updateTimerLengths(){
+        if(sessionlength < 0){
+            sessionlength = 0;
+        }
+        if(breaklength < 0){
+            breaklength = 0;
+        }
+
+        $("#session-length").html(sessionlength / 60);
+        $("#break-length").html(breaklength / 60);  
+    }
+
+    function init(){
+        breaklength = 5 * 60;
+        sessionlength = 25 * 60;
+        timeLeft = sessionlength;
+        clearInterval(timeInterval);
+        clearInterval(clickInterval);
+        if($(".status").hasClass('stop')){
+            $('.status').html("click to stop");
+            $('.status').removeClass('stop');
+            $('.status').addClass('go');
+        }
+        updateTimer();
+        updateTimerLengths();
+    }
+    
+    function iClickHandler(id){
+        switch (id){
+            case 'session-plus':
+                sessionlength += 60;
+                if(isSession){
+                    timeLeft += 60;
+                }
+                break;
+            case 'session-minus':
+                sessionlength -= 60;
+                if(isSession){
+                    timeLeft -= 60;
+                }
+                break;
+            case 'break-plus':
+                breaklength += 60;
+                if(!isSession){
+                    timeLeft += 60;
+                }
+                break;
+            case 'break-minus':
+                breaklength -= 60;
+                if(!isSession){
+                    timeLeft -= 60;
+                }
+                break;      
+        }
+        updateTimerLengths();
+        updateTimer();
+    }
+    
+    $(".operator").on("mousedown", function(){
+        var id = $(this).attr("id");
+        iClickHandler(id);
+        
+    });
     
     
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(function(position){
-            
-            $.getJSON("http://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.coords.longitude + "&sensor=true",function(json){
-                var location = "";
-                location = json.results[0].address_components[3].long_name;
-                //console.log(json.results[0].address_components[3].long_name);
-                $("#location").html(location);
-            });
-            
-            $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude,function(json){
-                var temperature = "";
-                var location = "";
-                console.log(json);
-                temperature += json.main.temp;
-                location = json.coord.lon + "," + json.coord.lat;
-                $("#temperature").html("today's temperature is: " + temperature);
-                   
-            });             
-        });
-    };  
+    $("#pomodoro").click(function(){
+        if($(".status").hasClass("go")){
+            $(".status").html('click to stop');
+            $(".status").removeClass('go');
+            $(".status").addClass('stop');
+            timeInterval = setInterval(function(){
+                timeLeft--;
+                updateTimer();
+            },1000);
+        } else {
+            clearInterval(timeInterval);
+            $('.status').html("click to start");
+            $('.status').removeClass('stop');
+            $('.status').addClass('go');
+        }
+    });
+    
+    $('#reset-pomodoro').click(init);
+    
+    $('#next-pomodoro').click(function(){
+        switchSession(false);
+        updateTimer();
+    })
 });
+
